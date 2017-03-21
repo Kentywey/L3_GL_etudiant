4ed93732aa9366fea88338b410bac1cbd8d633da
Kentywey <kweymeesch@gmail.com>
2017-03-21 13:42:22 +0100

    Ajout de clean.sh et de compilation

---
+++ TP_compilation/sayHello/compilation.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+g++ -std=c++11 -Wall -Wextra -g -o sayHello.out $1 $2
+
+
+
--- TP_compilation/sayHello/module1.hpp
+++ TP_compilation/sayHello/module1.hpp
@@ -1,9 +1,5 @@
 #ifndef _MODULE1_HPP_
 #define _MODULE1_HPP_
-
 #include <string>
-
 std::string getHello(int n);
-
 #endif
-
